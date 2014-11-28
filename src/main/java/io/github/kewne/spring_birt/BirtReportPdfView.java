3627190d819359c39f58d7185086f12adf04f3fe
kewne <joao.r.cabrita@gmail.com>
2014-11-28 13:29:18 +0000

    Added fallback to classpath resource.

--- src/main/java/io/github/kewne/spring_birt/BirtReportPdfView.java
+++ src/main/java/io/github/kewne/spring_birt/BirtReportPdfView.java
@@ -1,10 +1,13 @@
 package io.github.kewne.spring_birt;
 
 import org.eclipse.birt.report.engine.api.*;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.servlet.view.AbstractUrlBasedView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
@@ -29,6 +32,9 @@ public class BirtReportPdfView extends AbstractUrlBasedView {
     protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         String reportPath = getServletContext()
                 .getRealPath(getUrl());
+        if (!new File(reportPath).exists()) {
+            reportPath = new ClassPathResource(getUrl()).getFile().getAbsolutePath();
+        }
         IReportRunnable report = reportEngine.openReportDesign(reportPath);
         IRunAndRenderTask task = reportEngine.createRunAndRenderTask(report);
 
@@ -36,14 +42,15 @@ public class BirtReportPdfView extends AbstractUrlBasedView {
         for (Map.Entry<String, Object> e : model.entrySet()) {
             task.setParameterValue(e.getKey(), e.getValue());
         }
-        RenderOption opts = buildRenderOptions(response);
+        RenderOption opts = buildRenderOptions(request, response);
         task.setRenderOption(opts);
 
         task.run();
         task.close();
     }
 
-    private RenderOption buildRenderOptions(HttpServletResponse response) throws IOException {
+    private RenderOption buildRenderOptions(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ServletServerHttpRequest goodRequest = new ServletServerHttpRequest(request);
         RenderOption opts = new PDFRenderOption();
         opts.setOutputFormat("PDF");
         opts.setOutputStream(response.getOutputStream());
