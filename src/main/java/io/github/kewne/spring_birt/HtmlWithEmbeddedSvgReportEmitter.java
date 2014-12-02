378db7d7c17723bf5b626ca23b99d11b0f1f5e16
kewne <joao.r.cabrita@gmail.com>
2014-12-02 18:32:03 +0000

    Fixed issue that caused image to be positioned incorrectly.

--- src/main/java/io/github/kewne/spring_birt/HtmlWithEmbeddedSvgReportEmitter.java
+++ src/main/java/io/github/kewne/spring_birt/HtmlWithEmbeddedSvgReportEmitter.java
@@ -3,16 +3,12 @@ package io.github.kewne.spring_birt;
 import org.eclipse.birt.report.engine.content.IImageContent;
 import org.eclipse.birt.report.engine.emitter.html.HTMLReportEmitter;
 
-import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class HtmlWithEmbeddedSvgReportEmitter extends HTMLReportEmitter {
 
     @Override
     protected void outputSVGImage(IImageContent image, StringBuffer styleBuffer, int display) {
-        try {
-            out.write(image.getData());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        writer.cdata(new String(image.getData(), StandardCharsets.UTF_8));
     }
 }
